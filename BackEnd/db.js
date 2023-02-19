9cec73fb298ba4fc90997ac9b5e22465cf34304a
Ayush Kr Shanu <112754570+Ayush-kr-shanu@users.noreply.github.com>
2023-02-19 20:02:57 +0530

    backend

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 require('dotenv').config()
 
-const connection=mongoose.connect('mongodb+srv://ayush:ayush@cluster0.lo4qsjy.mongodb.net/notesApi?retryWrites=true&w=majority')
+const connection=mongoose.connect(process.env.url)
 
 module.exports={
     connection
